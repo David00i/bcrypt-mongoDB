2cab1f7a6d55d641a03e83ee75a46c81231c8666
David 00i <ooidavid10@gmail.com>
2022-05-04 14:10:10 +0800

    initial commit

---
+++ hash.js
@@ -0,0 +1,21 @@
+const bcrypt = require("bcryptjs")
+const saltRounds = 10;
+
+//let faker = require('./faker.js');
+const password = "mypass123";
+bcrypt.genSalt(saltRounds, function (saltError, salt) {
+  if (saltError) {
+    throw saltError
+  } else {
+    bcrypt.hash(password, salt, function(hashError, hash) {
+      if (hashError) {
+        throw hashError
+      } else {
+        console.log(hash)
+        //$2a$10$FEBywZh8u9M0Cec/0mWep.1kXrwKeiWDba6tdKvDfEBjyePJnDT7K
+      }
+    })
+  }
+})
+const convert = bcrypt.hashSync(password, saltRounds) 
+module.exports = {convert}
